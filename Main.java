3e4e6a701b78735df406b154ba2ec0dcc972a0ae
taku <t.handa.sys23@morijyobi.ac.jp>
2023-12-22 11:42:54 +0900

    Main.javaの修正

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
-public class Main{																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																	
-	
+
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    	System.out.print("１人目の身長を入力してください");
-        double height = scanner.nextDouble();
-        System.out.print("１人目体重を入力してください");
+
+        System.out.print("１人目の体重（kg）を入力してください: ");
         double weight = scanner.nextDouble();
-        scanner.close();
-        System.out.printf("１人目のBMI値は%.2fです%n", weight / Math.pow(height / 100, 2));
+        System.out.print("1人目の身長（m）を入力してください: ");
+        double height = scanner.nextDouble();
+        double bmi = weight / (height * height);
+        System.out.printf("あなたのBMIは %.2f です。\n", bmi);
+
+        System.out.print("2人目の体重（kg）を入力してください: ");
+        double weight2 = scanner.nextDouble();
+        System.out.print("2人目の身長（m）を入力してください: ");
+        double height2 = scanner.nextDouble();
+        double bmi2 = weight2 / (height2 * height2);
+        System.out.printf("あなたのBMIは %.2f です。\n", bmi2);
     }
 }
-
-
-
